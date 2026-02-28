3a5dd9c345e6ab709b7e6c3ac36ce7f2cd7e0bac
Your Name <your_email@example.com>
2026-03-01 01:40:19 +0200

    Refactor: Made el_dahih_pro_app the primary app directory containing the bug fixes

--- el_dahih_pro_app/encryption_utils.py
+++ el_dahih_pro_app/encryption_utils.py
@@ -6,7 +6,7 @@ import json
 import logging
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.backends import default_backend
 import base64
 
@@ -24,7 +24,7 @@ class EncryptionManager:
         if salt is None:
             salt = os.urandom(16)
         
-        kdf = PBKDF2(
+        kdf = PBKDF2HMAC(
             algorithm=hashes.SHA256(),
             length=32,
             salt=salt,
